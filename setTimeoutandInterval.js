5009e3a44d981d22cfe4298d90f9ecee9cb41e87
sahil <sahil.tripathi2898@gmail.com>
2021-04-03 23:29:09 +0530

    Set Interval

--- setTimeoutandInterval.js
+++ setTimeoutandInterval.js
@@ -1,12 +1,41 @@
 //setTimeout
+
 function call(first, second) {
 	console.log(first, second);
 }
 //Function name , time , parameters
-setTimeout(call, 1000);
+setTimeout(call, 1000, 'Sahil', 'Tripathi');
+
 //setTimeout(call()) --- Wrong don't call the function just pass the function
 
 //With arrow function we cannot pass arguements
 setTimeout(() => {
 	console.log('Arrow Function');
 }, 1000);
+
+let timeOut = setTimeout(() => {
+	console.log('Arrow Function');
+}, 1000);
+clearTimeout(timeOut);
+console.log(timeOut);
+
+//SetInterval
+let intervalOut = setInterval(() => {
+	console.log('interval');
+}, 1000);
+
+setTimeout(() => {
+	clearInterval(intervalOut);
+}, 3000);
+
+//
+let delay = 1000;
+
+let timer = setTimeout(function request() {
+	//do something
+
+	if (error) {
+		delay *= 2;
+	}
+	timer = setTimeout(request, delay);
+}, delay);
