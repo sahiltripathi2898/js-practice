a76523f557bc5a9b461333dd6b9862d26dbb3647
sahil <sahil.tripathi2898@gmail.com>
2021-04-11 23:20:23 +0530

    Debounce 2

--- debounce.js
+++ debounce.js
@@ -36,3 +36,18 @@ function saveInput() {
 	console.log('Saving data');
 }
 const processChange = debounce(() => saveInput());
+
+/////
+const debounce = (func, wait) => {
+	let timeout;
+
+	return function executedFunction(...args) {
+		const later = () => {
+			clearTimeout(timeout);
+			func(...args);
+		};
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
+};
