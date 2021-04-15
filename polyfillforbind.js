b3a2665af098e9de8e859448a694eaedbd352f9c
sahil <sahil.tripathi2898@gmail.com>
2021-04-15 16:05:26 +0530

    Self made bind without arguements

--- polyfillforbind.js
+++ polyfillforbind.js
@@ -1,7 +1,7 @@
 ///// Polyfill for Bind function
 // Making own bind function , because bind may not be available in some browsers
 
-let obj1 = {
+let name = {
 	firstName: 'Sahil',
 	lastName: 'Tripathi',
 };
@@ -10,5 +10,23 @@ let printName = function () {
 	console.log(this.firstName + ' ' + this.lastName);
 };
 
-let normalBind = printName.bind(obj1);
+// Normal bind function
+let normalBind = printName.bind(name);
 normalBind();
+
+// Self made bind function
+// What we want -> let selfBind = printName.myBind(name);
+
+// myBind should be available to all functions , so add it in functions prototypre
+Function.prototype.myBind = function (...args) {
+	// here 'this' will point to the printName method which is called upon
+	let currMethod = this;
+
+	// Will return a function , since bind functions return a function
+	return function () {
+		currMethod.call(args[0]);
+	};
+};
+
+let selfBind = printName.myBind(name);
+selfBind();
