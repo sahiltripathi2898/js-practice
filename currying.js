a5206deef5fb18b5a42450d1660cace09d3ddc1d
sahil <sahil.tripathi2898@gmail.com>
2021-04-17 13:09:28 +0530

    Currying using closure

--- currying.js
+++ currying.js
@@ -6,8 +6,11 @@ let multiply = function (x, y) {
 	console.log(x * y);
 };
 
-let multiplyByTwo = multiply.bind(this, 2); // x=3
+let multiplyByTwo = multiply.bind(this, 2); // x=2
 multiplyByTwo(3); //y=3 , will output 6
+
+let multiplyByThree = multiply.bind(this, 3); // x=3
+multiplyByThree(4); //y=4 , will output 12
 
 // Ex 2 using Closure
 
@@ -19,3 +22,6 @@ let addition = function (x) {
 
 let additionByTwo = addition(2); // x=2
 additionByTwo(3); //y=3 , output is 5
+
+let additionByThree = addition(3); // x=3
+additionByThree(4); //y=4 , output is 7
