d770283d458c1a722f172536b5242281d43bba0a
sahil <sahil.tripathi2898@gmail.com>
2021-04-18 18:32:26 +0530

    Examples of closure

---
+++ closure.js
@@ -0,0 +1,11 @@
+// Examples of closures
+
+function init() {
+	var name = 'Mozilla'; // name is a local variable created by init
+	function displayName() {
+		// displayName() is the inner function, a closure
+		alert(name); // use variable declared in the parent function
+	}
+	displayName();
+}
+init();
